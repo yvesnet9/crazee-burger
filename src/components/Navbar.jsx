1ce962a1df6222c3ade38592209724ed277d9408
Yves <yvesnet9@gmail.com>
2025-09-26 17:35:50 +0200

    F04: ajout de la Navbar avec styled-components et liens de navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const NavbarContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: #333;
+  color: white;
+`;
+
+const Logo = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  a {
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+function Navbar() {
+  return (
+    <NavbarContainer>
+      <Logo>🍔 CrazeeBurger</Logo>
+      <NavLinks>
+        <Link to="/login">Login</Link>
+        <Link to="/order">Commander</Link>
+      </NavLinks>
+    </NavbarContainer>
+  );
+}
+
+export default Navbar;
